3fc4d269a6290179535432604735413fd92e61a4
Probotmaker8 <101275268+Probotmaker8@users.noreply.github.com>
2022-05-26 14:18:29 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Probotmaker8/stella-bot/stella-bot.git /stella-bot
+  git clone https://github.com/Probotmaker8/stella-bot.git /stella-bot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /stella-bot
