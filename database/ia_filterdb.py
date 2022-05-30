1cb88bcf0362368580664e18d98a705351f9eec0
thetix7 <88897640+thetix7@users.noreply.github.com>
2022-05-30 08:17:33 +0530

    Update ia_filterdb.py

--- database/ia_filterdb.py
+++ database/ia_filterdb.py
@@ -89,7 +89,21 @@ async def get_search_results(query, file_type=None, max_results=10, offset=0, fi
         return []
 
     if USE_CAPTION_FILTER:
-        filter = {'$or': [{'file_name': regex}, {'caption': regex}]}
+        filter = {'$or': [{'file_name': regex}, {'caption': 🏷 Title: <a href={url}>{title}</a>
+        🎭 Genres: {genres}
+        📆 Year: <a href={url}/releaseinfo>{year}</a>
+        🌟 Rating: <a href={url}/ratings>{rating}</a> / 10
+
+
+        ------------🖤------------
+
+        🎞ᴄʜᴀɴɴᴇʟ - @CinemaVeedMovies
+                                ⛕ 
+        👥ɢʀᴏᴜᴘ    - @CinemaVeed
+
+        🔗 > 𝘑𝘰𝘪𝘯
+        🔗 > 𝘚𝘩𝘢𝘳𝘦
+        🔗 > 𝘚𝘶𝘱𝘱𝘰𝘳𝘵}]}
     else:
         filter = {'file_name': regex}
 
