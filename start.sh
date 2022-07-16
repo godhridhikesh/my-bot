e92825b5f44f15cba1093029283b617835ba63d4
ᴍᴇ!!! <107136639+botxeditor@users.noreply.github.com>
2022-07-16 15:43:19 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/4kcinemas/My-Dream.git /My-Dream
+  git clone https://github.com/botxeditor/Nazriyafilterbot- /My-Dream
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /My-Dream
 fi
 cd /My-Dream
 pip3 install -U -r requirements.txt
-echo "Starting Bot...."
+echo "Starting Bot [VBOTz]...."
 python3 bot.py
